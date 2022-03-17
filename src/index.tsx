c5f72c9e2e4b5a300212477c24e3f04445c1eb5b
Mike Berman <mike@berman.xyz>
2022-03-17 09:23:05 +0000

    Add MUI & emotion

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './react/App';
+import { Global, css } from '@emotion/react'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        :root {
+          font-family: Roboto, sans-serif;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
